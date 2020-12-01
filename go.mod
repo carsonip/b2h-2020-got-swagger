0aec779b3b8c8c873116931ae947828ee90583f2
Tymon Solecki <tymon.solecki@pendo.io>
2020-12-01 16:24:17 +0000

    make a command line tool
    * make the "main.go" into a CLI tool
    * the tool prints out only last handler for a match

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
+	github.com/devfacet/gocmd v3.1.0+incompatible
 	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab
 	github.com/stretchr/testify v1.6.1
 )
--- main.go
+++ main.go
@@ -3,16 +3,54 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/devfacet/gocmd"
 	"io/ioutil"
-	"log"
 	"martiniExample/hack"
+	"strings"
 )
 
 func main() {
-	printRoutes()
+	//printRoutes()
 	//exportRoutes()
+	routes := "/Users/tymon.solecki/dev/pendo-appengine/src/routes.json"
+	//matchRoute(routes, "get", "/api/s/5668600916475904/subscription/setting/featureFlags")
+	// Init the app
+	flags := struct {
+		export struct {
+			Settings bool `settings:"true" allow-unknown-arg:"true"`
+		} `command:"export" description:"Print arguments"`
+		Match struct {
+			Settings bool `settings:"true" allow-unknown-arg:"true"`
+		} `command:"match" description:"Print arguments"`
+		Echo      struct {
+			Settings bool `settings:"true" allow-unknown-arg:"true"`
+		} `command:"echo" description:"Print arguments"`
 
-	//matchRoute("./routes.json", "get", "/api/s/5668600916475904/subscription/setting/featureFlags")
+	}{}
+
+	gocmd.HandleFlag("export", func(cmd *gocmd.Cmd, args []string) error {
+		fmt.Printf("exporting to routes.json...")
+		exportRoutes()
+		return nil
+	})
+	gocmd.HandleFlag("Echo", func(cmd *gocmd.Cmd, args []string) error {
+		fmt.Printf("%s\n", strings.Join(cmd.FlagArgs("Echo")[1:], " "))
+		return nil
+	})
+
+
+	gocmd.HandleFlag("Match", func(cmd *gocmd.Cmd, args []string) error {
+		matchRoute(routes, cmd.FlagArgs("Match")[1], cmd.FlagArgs("Match")[2])
+		return nil
+	})
+
+	gocmd.New(gocmd.Options{
+		Name:        "basic",
+		Version:     "1.0.0",
+		Description: "A basic app",
+		Flags:       &flags,
+		ConfigType:  gocmd.ConfigTypeAuto,
+	})
 }
 
 func matchRoute(path string, method string, match string) {
@@ -22,11 +60,8 @@ func matchRoute(path string, method string, match string) {
 		routeDefs := hack.RouteDefinitions{}
 		json.Unmarshal([]byte(dat), &routeDefs)
 		rMatch := routeDefs.MatchPath(method, match)
-		rJSON, err := json.Marshal(rMatch)
-		if err != nil {
-			log.Fatalf(err.Error())
-		}
-		fmt.Printf("%s\n", string(rJSON))
+		lastHandler := rMatch.Handlers[len(rMatch.Handlers)-1]
+		fmt.Printf("%v %v\n%v:%v", rMatch.Method, rMatch.Route, lastHandler.Path, lastHandler.LineNo)
 	}
 }
 
